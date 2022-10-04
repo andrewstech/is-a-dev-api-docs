caf7be5c0d27e0c93c484fecb4e3dc017222e644
andrewstech <info@andrewstech.me>
2022-10-04 19:34:13 +0100

    Update docusaurus.config.js

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -37,7 +37,7 @@ const config = {
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           editUrl:
-            'https://github.com/andrewstech/is-a-dev-api-docs/edit/main/docs/',
+            'https://github.com/andrewstech/is-a-dev-api-docs/edit/main/',
         },
         theme: {
           customCss: require.resolve('./src/css/custom.css'),
